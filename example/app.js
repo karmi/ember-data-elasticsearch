b479f35460ed347c4099b3c33880e0d8b4a4c08d
Karel Minarik <karmi@karmi.cz>
2012-08-14 14:13:32 +0200

    [EXAMPLE] Added the elasticsearch persistence to the example application

--- example/app.js
+++ example/app.js
@@ -3,26 +3,70 @@ var App = Em.Application.create({
 
   Models:      Ember.Object.extend(),
   Views:       Ember.Object.extend(),
-  Controllers: Ember.Object.extend()
+  Controllers: Ember.Object.extend(),
+
+  // Sniff the URL if we're running as an elasticsearch `_site` plugin
+  //
+  elasticsearch_url: function() {
+    var location = window.location
+    return (/_plugin/.test(location.href.toString())) ? location.protocol + "//" + location.host : "http://localhost:9200"
+  }(),
+
+  ready: function() {
+    var index_url = [App.store.adapter.url, "tasks"].join('/');
+
+    // Let's check if the `tasks` index exists and create it otherwise.
+    //
+    jQuery.ajax({
+      url:   index_url,
+      type:  'HEAD',
+      error: function() { jQuery.post(index_url, {}, function(data) {}) }
+    });
+  }
 });
 
-App.Models.Task = Ember.Object.extend({
-  title:     null,
-  completed: false
+// Define a store for the application
+//
+App.store = DS.Store.create({
+  revision: 4,
+  adapter: DS.ElasticSearchAdapter.create({url: App.elasticsearch_url})
+});
+
+// Define the model using _Ember Data_ API
+//
+App.Models.Task = DS.Model.extend({
+  // Properties:
+  //
+  title:     DS.attr('string'),
+  completed: DS.attr('boolean'),
+
+  // Observe changes and persist them in elasticsearch
+  //
+  changed: function() {
+    App.store.commit()
+  }.observes('completed')
+});
+
+App.Models.Task.reopenClass({
+  // Define the index and type for elasticsearch
+  //
+  url: 'tasks/task'
 });
 
 App.Controllers.tasks = Ember.ArrayController.create({
-  content: [],
+  content: App.Models.Task.find(),
 
   createTask: function(value) {
-    var task = App.Models.Task.create({ title: value });
-    this.pushObject(task);
+    var task = App.Models.Task.createRecord({ title: value });
+    App.store.commit();
   },
 
   removeTask: function(event) {
     if ( confirm("Delete this task?") ) {
       var task = event.context;
-      this.removeObject(task);
+
+      task.deleteRecord();
+      App.store.commit();
     }
   },
 
